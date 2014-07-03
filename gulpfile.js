d6da6e434e01a14afdb6a6302c15a291d30a8235
Justin Chmura <justin.chmura@gmail.com>
2014-07-03 10:39:40 -0700

    Created default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,18 @@
 
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('jshint', function () {
-  return gulp.src(['*.js']).pipe(jshint());
+  gulp.src(['*.js']).pipe(jshint());
+});
+
+gulp.task('default', ['jshint'], function () {
+  gulp.src(['treeview.js'])
+    .pipe(gulp.dest('./dist'))
+    .pipe(uglify())
+    .pipe(rename('treeview.min.js'))
+    .pipe(gulp.dest('./dist'));
+  gulp.src(['treeview.css']).pipe(gulp.dest('./dist'));
 });
