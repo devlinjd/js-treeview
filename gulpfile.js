0ecdead62e2c8e95f232fed5f93631e734aa1917
Justin Chmura <justin.chmura@gmail.com>
2014-11-20 07:52:36 -0700

    Updated Gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,60 @@
 'use strict';
 
 var gulp = require('gulp');
+
 var jshint = require('gulp-jshint');
+var csslint = require('gulp-csslint');
 var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var header = require('gulp-header');
+var qunit = require('gulp-qunit');
+
+var pkg = require('./package.json');
+var scripts = ['./src/*.js'];
+var css = ['./src/*.css'];
+var build = './dist';
+
+var banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @license <%= pkg.license %>',
+  ' */',
+''].join('\n');
 
 gulp.task('jshint', function () {
-  gulp.src(['*.js']).pipe(jshint());
+  return gulp.src(scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('default', ['jshint'], function () {
-  gulp.src(['treeview.js'])
-    .pipe(gulp.dest('./dist'))
+gulp.task('jsmin', function () {
+  return gulp.src(['./src/treeview.js'])
+    .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(rename('treeview.min.js'))
-    .pipe(gulp.dest('./dist'));
-  gulp.src(['treeview.css']).pipe(gulp.dest('./dist'));
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest(build));
 });
+
+gulp.task('cssmin', function () {
+  return gulp.src(['./src/treeview.css'])
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssmin())
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest(build));
+});
+
+gulp.task('test', ['jslint', 'csslint'], function () {
+  return gulp.src('./test/index.html').pipe(qunit());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(scripts, ['jslint']);
+  gulp.watch(css, ['csslint']);
+  gulp.watch(['./test/*.js', './test/*.html'], ['test']);
+});
+
+gulp.task('build', ['jsmin', 'cssmin']);
+gulp.task('default', ['test']);
